a107a88c87527222edaaa4cb7c39aadcad594898
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-10 02:59:47 +0000

    for wp3.1
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/tag-feed@2112 f53fe46e-8c02-459b-9fdd-93e75771b278

--- tag-feed.php
+++ tag-feed.php
@@ -1,14 +1,15 @@
 <?php
 /*
 Plugin Name: Tag Feed
-Plugin URI: 
+Plugin URI:
 Description: RSS2 feeds
 Author: Andrew Billits (Incsub)
-Version: 1.0.2
+Version: 2.0
 Author URI:
-*/ 
+WDP ID: 96
+*/
 
-/* 
+/*
 Copyright 2007-2009 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -29,6 +30,17 @@ if (empty($wp)) {
 	require_once('../wp-load.php');
 	wp('feed=rss2');
 }
+
+/* -------------------- Update Notifications Notice -------------------- */
+if ( !function_exists( 'wdp_un_check' ) ) {
+  add_action( 'admin_notices', 'wdp_un_check', 5 );
+  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+  function wdp_un_check() {
+    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+  }
+}
+/* --------------------------------------------------------------------- */
 
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
