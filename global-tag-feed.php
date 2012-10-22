1ac62b928289f4716b2dc98e58f81860a0ac46d6
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-10-22 11:14:24 +0000

    Updated for new version of postindexer Now uses a permalink URL for the feed rather than having to be placed in wp-content Old version needs updating due to conflict with recent checkin
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/tag-feed@7522 f53fe46e-8c02-459b-9fdd-93e75771b278

--- global-tag-feed.php
+++ global-tag-feed.php
@@ -6,7 +6,7 @@ Description: RSS2 feeds
 Version: 3.0
 Author: Barry (Incsub)
 Author URI:
-WDP ID: 96
+WDP ID:
 Network: true
 */
 
@@ -69,13 +69,29 @@ class globaltabfeed {
 
 	function do_global_tag_feed() {
 
-		die('boo');
+		global $network_query, $network_post;
 
-		header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
+		// Remove all excerpt more filters
+		remove_all_filters('excerpt_more');
+
+		$tag = $_GET['tag'];
+		if ( empty( $tag ) ) {
+			$tag = 'uncategorized';
+		}
+
+		@header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 		$more = 1;
 
-		echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
+		echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
 
+		// Even though this is the tag feed - we'll pull categories as well
+		if(network_term_is_tag( $tag)) {
+			$network_query = network_query_posts( array( 'taxonomy' => 'post_tag', 'term' => $tag ));
+		} else {
+			$network_query = network_query_posts( array( 'taxonomy' => 'category', 'term' => $tag ));
+		}
+
+		?>
 		<rss version="2.0"
 			xmlns:content="http://purl.org/rss/1.0/modules/content/"
 			xmlns:wfw="http://wellformedweb.org/CommentAPI/"
@@ -87,39 +103,39 @@ class globaltabfeed {
 		>
 
 		<channel>
-			<title><?php bloginfo_rss('name'); wp_title_rss(); ?></title>
+			<title><?php bloginfo_rss('name'); _e(' - Global Tag Feed For : ','postindexer'); echo strip_tags($tag); ?></title>
 			<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
 			<link><?php bloginfo_rss('url') ?></link>
 			<description><?php bloginfo_rss("description") ?></description>
-			<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_lastpostmodified('GMT'), false); ?></lastBuildDate>
+			<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_lastpostmodified('GMT'), false); ?></lastBuildDate>
 			<language><?php bloginfo_rss( 'language' ); ?></language>
 			<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
 			<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
 			<?php do_action('rss2_head'); ?>
-			<?php while( have_posts()) : the_post(); ?>
+			<?php while( network_have_posts()) : network_the_post(); ?>
 			<item>
-				<title><?php the_title_rss() ?></title>
-				<link><?php the_permalink_rss() ?></link>
-				<comments><?php comments_link_feed(); ?></comments>
-				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
-				<dc:creator><?php the_author() ?></dc:creator>
-				<?php the_category_rss('rss2') ?>
+				<title><?php network_the_title_rss(); ?></title>
+				<link><?php network_the_permalink_rss(); ?></link>
+				<comments><?php network_comments_link_feed(); ?></comments>
+				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+				<dc:creator><?php network_the_author(); ?></dc:creator>
+				<?php network_the_category_rss('rss2'); ?>
 
-				<guid isPermaLink="false"><?php the_guid(); ?></guid>
-		<?php if (get_option('rss_use_excerpt')) : ?>
-				<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
-		<?php else : ?>
-				<description><![CDATA[<?php the_excerpt_rss() ?>]]></description>
-			<?php if ( strlen( $post->post_content ) > 0 ) : ?>
-				<content:encoded><![CDATA[<?php the_content_feed('rss2') ?>]]></content:encoded>
-			<?php else : ?>
-				<content:encoded><![CDATA[<?php the_excerpt_rss() ?>]]></content:encoded>
-			<?php endif; ?>
-		<?php endif; ?>
-				<wfw:commentRss><?php echo esc_url( get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
-				<slash:comments><?php echo get_comments_number(); ?></slash:comments>
-		<?php rss_enclosure(); ?>
-			<?php do_action('rss2_item'); ?>
+				<guid isPermaLink="false"><?php network_the_guid(); ?></guid>
+		<?php if (get_option('rss_use_excerpt')) { ?>
+				<description><![CDATA[<?php network_the_excerpt_rss(); ?>]]></description>
+		<?php } else { ?>
+				<description><![CDATA[<?php network_the_excerpt_rss() ?>]]></description>
+			<?php if ( strlen( $network_post->post_content ) > 0 ) { ?>
+				<content:encoded><![CDATA[<?php network_the_content_feed('rss2'); ?>]]></content:encoded>
+			<?php } else { ?>
+				<content:encoded><![CDATA[<?php network_the_excerpt_rss(); ?>]]></content:encoded>
+			<?php } ?>
+		<?php } ?>
+				<wfw:commentRss><?php echo esc_url( network_get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
+				<slash:comments><?php echo network_get_comments_number(); ?></slash:comments>
+		<?php network_rss_enclosure(); ?>
+			<?php do_action('network_rss2_item'); ?>
 			</item>
 			<?php endwhile; ?>
 		</channel>
