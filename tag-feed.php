bf0aab9c781bbcd274b04e2adabb1fa04167bae5
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-10-25 15:32:39 +0000

    updated old version of tag feed with new updates
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/tag-feed@7547 f53fe46e-8c02-459b-9fdd-93e75771b278

--- tag-feed.php
+++ tag-feed.php
@@ -1,12 +1,6 @@
 <?php
 /*
-Plugin Name: Tag Feed
-Plugin URI:
-Description: RSS2 feeds
-Version: 2.1
-Author: Andrew Billits (Incsub) / S H Mohanjith (Incsub) / Barry (Incsub)
-Author URI:
-WDP ID: 96
+* The original method of getting the tag feed but using the new code - this should be replaced with the new plugin now
 */
 
 /*
@@ -31,17 +25,6 @@ if (empty($wp)) {
 	wp('feed=rss2');
 }
 
-/* -------------------- Update Notifications Notice -------------------- */
-if ( !function_exists( 'wdp_un_check' ) ) {
-  add_action( 'admin_notices', 'wdp_un_check', 5 );
-  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
-  function wdp_un_check() {
-    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
-      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
-  }
-}
-/* --------------------------------------------------------------------- */
-
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
@@ -54,95 +37,72 @@ $number = $_GET['number'];
 if ( empty( $number ) ) {
 	$number = '25';
 }
-//------------------------------------------------------------------------//
-//---Functions------------------------------------------------------------//
-//------------------------------------------------------------------------//
-$blog_public = isset($_GET['blog_public'])?$_GET['blog_public']:0;
-$tag_id = $wpdb->get_var("SELECT term_id FROM " . $wpdb->base_prefix . "site_terms WHERE slug = '" . $tag . "'");
 
-if ( empty( $tag_id ) || !is_numeric( $tag_id ) || $tag_id == 0 ) {
-	$tag_id = $wpdb->get_var("SELECT term_id FROM " . $wpdb->base_prefix . "site_terms WHERE name = '" . $tag . "'");
-}
+global $network_query, $network_post;
 
-$tag_sql = " AND post_terms LIKE '%|" . $tag_id . "|%' ";
+// Remove all excerpt more filters
+remove_all_filters('excerpt_more');
 
-$public_sql = "";
-if ( !empty($blog_public) ) {
-       $public_sql = " AND blog_public = '{$blog_public}' ";
-}
+@header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 
-$query = "SELECT * FROM " . $wpdb->base_prefix . "site_posts WHERE site_id = '" . $current_site->id . "' {$tag_sql} {$public_sql} ORDER BY post_published_gmt DESC LIMIT " . $number;
-
-$posts = $wpdb->get_results( $query, ARRAY_A );
-
-if ( count( $posts ) > 0 ) {
-	$last_published_post_date_time = $wpdb->get_var("SELECT post_published_gmt FROM " . $wpdb->base_prefix . "site_posts WHERE site_id = '" . $current_site->id . "' {$tag_sql} {$public_sql} ORDER BY post_published_gmt DESC LIMIT 1");
-}
-
-function tag_feed_trim_excerpt($text) {
-	$text = strip_shortcodes( $text );
-	
-	$text = apply_filters('the_content', $text);
-	$text = str_replace(']]>', ']]&gt;', $text);
-        $text = strip_tags($text);
-	$excerpt_length = apply_filters('excerpt_length', 55);
-        $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
-        $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
-        if ( count($words) > $excerpt_length ) {
-                array_pop($words);
-                $text = implode(' ', $words);
-                $text = $text . $excerpt_more;
-        } else {
-                $text = implode(' ', $words);
-        }
-
-	return $text;
-}
-
-header('HTTP/1.0 200 OK', true);
-header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
+echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
+
+// Even though this is the tag feed - we'll pull categories as well
+if(network_term_is_tag( $tag)) {
+	$network_query = network_query_posts( array( 'taxonomy' => 'post_tag', 'term' => $tag ));
+} else {
+	$network_query = network_query_posts( array( 'taxonomy' => 'category', 'term' => $tag ));
+}
+
 ?>
-<?php echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 <rss version="2.0"
 	xmlns:content="http://purl.org/rss/1.0/modules/content/"
 	xmlns:wfw="http://wellformedweb.org/CommentAPI/"
 	xmlns:dc="http://purl.org/dc/elements/1.1/"
 	xmlns:atom="http://www.w3.org/2005/Atom"
 	xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+	xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+	<?php do_action('rss2_ns'); ?>
 >
 
 <channel>
-	<title><![CDATA[<?php bloginfo_rss('name'); ?> <?php _e('Posts'); ?>]]></title>
+	<title><?php bloginfo_rss('name'); _e(' - Global Tag Feed For : ','postindexer'); echo strip_tags($tag); ?></title>
 	<atom:link href="<?php self_link(); ?>" rel="self" type="application/rss+xml" />
-	<link><![CDATA[<?php bloginfo_rss('url') ?>]]></link>
-	<description><![CDATA[<?php bloginfo_rss("description") ?>]]></description>
-	<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $last_published_post_date_time, false); ?></pubDate>
-	<?php the_generator( 'rss2' ); ?>
-	<language><?php echo get_option('rss_language'); ?></language>
-    <?php
-	//--------------------------------------------------------------------//
-	if ( count( $posts ) > 0 ) {
-		foreach ($posts as $post) {
-			$author_display_name = $wpdb->get_var("SELECT display_name FROM " . $wpdb->base_prefix . "users WHERE ID = '" . $post['post_author'] . "'");
-			?>
-			<item>
-				<title><?php echo apply_filters( 'the_title_rss', $post['post_title']); ?></title>
-				<link><![CDATA[<?php echo $post['post_permalink']; ?>]]></link>
-				<comments><![CDATA[<?php echo $post['post_permalink'] . '#comments'; ?>]]></comments>
-				<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', $post['post_published_gmt'], false); ?></pubDate>
-				<dc:creator><?php echo $author_display_name; ?></dc:creator>
-				<guid isPermaLink="false"><?php echo $post['post_permalink']; ?></guid>
-                <description><![CDATA[<?php echo apply_filters('the_excerpt_rss', tag_feed_trim_excerpt($post['post_content'])); ?>]]></description>
-                <content:encoded><![CDATA[<?php echo apply_filters('the_content_feed', $post['post_content'], 'rss2'); ?>]]></content:encoded>
-				<wfw:commentRss><?php echo $post['post_permalink'] . 'feed/'; ?></wfw:commentRss>
-			</item>
-			<?php
-		}
-	}
-	//--------------------------------------------------------------------//
-	?>
+	<link><?php bloginfo_rss('url') ?></link>
+	<description><?php bloginfo_rss("description") ?></description>
+	<lastBuildDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_lastpostmodified('GMT'), false); ?></lastBuildDate>
+	<language><?php bloginfo_rss( 'language' ); ?></language>
+	<sy:updatePeriod><?php echo apply_filters( 'rss_update_period', 'hourly' ); ?></sy:updatePeriod>
+	<sy:updateFrequency><?php echo apply_filters( 'rss_update_frequency', '1' ); ?></sy:updateFrequency>
+	<?php do_action('rss2_head'); ?>
+	<?php while( network_have_posts()) : network_the_post(); ?>
+	<item>
+		<title><?php network_the_title_rss(); ?></title>
+		<link><?php network_the_permalink_rss(); ?></link>
+		<comments><?php network_comments_link_feed(); ?></comments>
+		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', network_get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+		<dc:creator><?php network_the_author(); ?></dc:creator>
+		<?php network_the_category_rss('rss2'); ?>
+
+		<guid isPermaLink="false"><?php network_the_guid(); ?></guid>
+<?php if (get_option('rss_use_excerpt')) { ?>
+		<description><![CDATA[<?php network_the_excerpt_rss(); ?>]]></description>
+<?php } else { ?>
+		<description><![CDATA[<?php network_the_excerpt_rss() ?>]]></description>
+	<?php if ( strlen( $network_post->post_content ) > 0 ) { ?>
+		<content:encoded><![CDATA[<?php network_the_content_feed('rss2'); ?>]]></content:encoded>
+	<?php } else { ?>
+		<content:encoded><![CDATA[<?php network_the_excerpt_rss(); ?>]]></content:encoded>
+	<?php } ?>
+<?php } ?>
+		<wfw:commentRss><?php echo esc_url( network_get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
+		<slash:comments><?php echo network_get_comments_number(); ?></slash:comments>
+<?php network_rss_enclosure(); ?>
+	<?php do_action('network_rss2_item'); ?>
+	</item>
+	<?php endwhile; ?>
 </channel>
 </rss>
 <?php
